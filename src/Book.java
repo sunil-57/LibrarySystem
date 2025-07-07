91236e547a69f51a9233d3dc3b017623fd411c18
Sunil Thapa <sunilthapa056@outlook.com>
2025-07-07 12:13:48 +0545

    initial commit

---
+++ src/Book.java
@@ -0,0 +1,14 @@
+public class Book {
+    //attributes ->book number, nam, quantity, author
+    int bookNuber;
+    String bookName;
+    int bookQuantity;
+    String bookAuthor;
+    //behaviour -> increase quantity, decrease quantity
+    public void increaseQuantity(){
+        System.out.println("need to work to increase");
+    }
+    public void decreaseQuantity(){
+        System.out.println("need to work to decrease");
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,17 @@
+public class User {
+    //attributes -> nam, username, contact
+    String name;
+    String username;
+    int contact;
+
+    //behavior -> check is registered?, borrow, return
+    public void isRegistered(){
+        System.out.println("need to work on checking registered?");
+    }
+    public void borrowBook(){
+        System.out.println("need to work on borrow book");
+    }
+    public void returnBook(){
+        System.out.println("need to work on return book");
+    }
+}
